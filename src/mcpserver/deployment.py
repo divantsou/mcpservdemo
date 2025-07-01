e4a2cd0dfd281457298cccd84c440ffe332fcf24
dzmitry.ivantsou <divantsou@splunk.com>
2025-07-01 15:48:38 +0200

    add mcp annotation to the add method

--- src/mcpserver/deployment.py
+++ src/mcpserver/deployment.py
@@ -3,6 +3,7 @@ from mcp.server.fastmcp import FastMCP
 # Create an MCP server
 mcp = FastMCP("Addition Demo")
 
+@mcp.tool()
 def add_two_numbers(a: int, b: int) -> int:
     """
     Add two numbers together.
